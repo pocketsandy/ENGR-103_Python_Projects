53bd57599d0e33fc0eae13d1814a9bb342d32a81
pocketsandy <146877250+pocketsandy@users.noreply.github.com>
2023-10-09 11:06:19 -0700

    Delete average.py

--- average.py
+++
@@ -1,9 +0,0 @@
-print("Please enter five numbers.")
-
-first_num = float(input("First Number:"))
-second_num = float(input("Second Number:"))
-third_num = float(input("Third Number:"))
-fourth_num = float(input("Fourth Number:"))
-fifth_num = float(input("Fifth Number:"))
-
-print("The average of those numbers is",(first_num+second_num+third_num+fourth_num+fifth_num)/5)
